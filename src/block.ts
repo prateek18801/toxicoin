3549e0c4fbe4be2af0ef64ca107acc980e557a20
Prateek Chaurasia <prateek18801@gmail.com>
2023-04-05 14:53:46 +0530

    Change block data to transactions

--- src/block.ts
+++ src/block.ts
@@ -1,17 +1,17 @@
+import Transaction from './transcation';
 import { SHA256 } from './utils';
 
-type blockdata = string;
 const _dif: number = 4;
 
 class Block {
-    private data: blockdata;
+    private transactions: Transaction[];
     private prev: string;
     private hash: string;
     private nonce: number;
     private timestamp: number;
 
-    constructor(data: blockdata, prev: string, timestamp: number) {
-        this.data = data;
+    constructor(transactions: Transaction[], prev: string, timestamp: number) {
+        this.transactions = transactions;
         this.hash = '';
         this.prev = prev;
         this.nonce = 0;
@@ -20,11 +20,17 @@ class Block {
     }
 
     getHash(): string { return this.hash; }
+    getPrev(): string { return this.prev; }
+    getTransactions(): Transaction[] { return this.transactions; }
+
+    genHash(): string {
+        return SHA256(JSON.stringify({ transactions: this.transactions, nonce: this.nonce, prev: this.prev, timestamp: this.timestamp }))
+    }
 
     mine(): void {
         while (this.hash.substring(0, _dif) !== Array(_dif + 1).join('0')) {
             this.nonce++;
-            this.hash = SHA256(JSON.stringify({ data: this.data, nonce: this.nonce, prev: this.prev, timestamp: this.timestamp }));
+            this.hash = this.genHash();
         }
     }
 };
