7b4870f79542f64287dbdf50950ac20883afe9cf
Prateek Chaurasia <prateek18801@gmail.com>
2023-04-05 15:11:06 +0530

    Add pending transactions and mining rewards

--- src/blockchain.ts
+++ src/blockchain.ts
@@ -1,18 +1,40 @@
 import Block from './block';
+import Transaction from './transcation';
 
 class Blockchain {
     private chain: Block[];
+    private reward: number;
+    private pendingTxn: Transaction[];
 
     constructor() {
         this.chain = [this.genesis()];
+        this.reward = 1;
+        this.pendingTxn = [];
     }
 
     latest(): Block { return this.chain[this.chain.length - 1]; }
-    genesis(): Block { return new Block('0', '0', 0); }
+    genesis(): Block { return new Block([new Transaction('0', '0', 0)], '0', 0); }
 
-    addBlock(data: string): void {
-        this.chain.push(new Block(data, this.latest().getHash(), Date.now()));
+    minePendingTxn(minerWalletAddress: string) {
+        this.chain.push(new Block(this.pendingTxn, this.latest().getHash(), Date.now()));
+        this.pendingTxn = [new Transaction('toxicoin', minerWalletAddress, this.reward)];
     }
+
+    createNewTxn(transcation: Transaction) {
+        this.pendingTxn.push(transcation);
+    }
+
+    calculateBalanceForWallet(walletAddress: string) {
+        let balance: number = 0;
+        for (const block of this.chain) {
+            for (const txn of block.getTransactions()) {
+                if (txn.from === walletAddress) balance -= txn.amount;
+                if (txn.to === walletAddress) balance += txn.amount;
+            }
+        }
+        return balance;
+    }
+
 };
 
 export default Blockchain;
