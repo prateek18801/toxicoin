2b36b21948d9d01ffccf315bef6b3d69bfbb8934
Prateek Chaurasia <prateek18801@gmail.com>
2023-04-05 15:13:29 +0530

    Update sample blockchain

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,16 @@
-import Blockchain from "./blockchain";
+import Blockchain from './blockchain';
+import Transaction from './transcation';
 
 const toxichain = new Blockchain();
-console.log('mining 1');
-toxichain.addBlock('block 1 data');
-console.log('mining 2');
-toxichain.addBlock('block 2 data');
+toxichain.createNewTxn(new Transaction('user_1', 'user_2', 100));
+toxichain.createNewTxn(new Transaction('user_2', 'user_1', 50));
+toxichain.minePendingTxn('miner_1');
+toxichain.createNewTxn(new Transaction('user_2', 'user_1', 25));
+toxichain.minePendingTxn('miner_1');
 
+console.log(JSON.stringify(toxichain));
 console.log(toxichain);
 
+console.log(toxichain.calculateBalanceForWallet('user_1'));
+console.log(toxichain.calculateBalanceForWallet('user_2'));
+console.log(toxichain.calculateBalanceForWallet('miner_1'));
